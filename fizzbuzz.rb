5253d5ef3d2ca05246a30382d9caa63068236ca9
Leon Malisov <leonmalisov@gmail.com>
2019-11-29 16:05:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-
+def fizzbuzz(num)
+  if (num % 3 = 0 && num % 5 = 0)
+    "FizzBuzz" 
+  end
+  elsif num % 3 = 0
+    "Fizz" 
+  end
+  elsif num % 5 = 0
+    "Buzz" 
+  end
+  elsif (num % 3 != 0 && num % 5 != 0)
+    "FizzBuzz" 
+  end
+  
 end
